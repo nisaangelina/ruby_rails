f1371205717ed6e8281ebbc1156e04090fee8726
nisaangelina <nisa.angelina@ukirama.com>
2018-09-20 11:00:13 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
